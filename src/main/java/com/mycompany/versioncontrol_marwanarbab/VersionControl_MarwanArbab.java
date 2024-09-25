35537f26bc0d36d0b2a043564d57261e5625b2ed
marwanarbab <marwanarbab@localhost>
2024-09-25 17:02:10 -0500

    initial commit

---
+++ src/main/java/com/mycompany/versioncontrol_marwanarbab/VersionControl_MarwanArbab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_marwanarbab;
+
+/**
+ *
+ * @author marwanarbab
+ */
+public class VersionControl_MarwanArbab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
